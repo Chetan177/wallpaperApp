8580da096b026a1da500c0d3cfd5750030a468cb
chetan177 <chetanpandey177@gmail.com>
2020-09-29 21:08:00 +0530

    widget changes

--- lib/widgets/widget.dart
+++ lib/widgets/widget.dart
@@ -2,21 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:wallpaperApp/model/wallpaper_model.dart';
 
 Widget brandName() {
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: <Widget>[
-      Text(
-        "Wall",
-        style: TextStyle(color: Colors.black87),
+  return  RichText(
+    textAlign: TextAlign.center,
+    text: TextSpan(
+      style: TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.w500,
       ),
-      Text(
-        "Papelz",
-        style: TextStyle(color: Colors.blue),
-      )
-    ],
+      children: <TextSpan>[
+        TextSpan(text: 'Wall ', style: TextStyle(color: Colors.black87)),
+        TextSpan(text: 'Papzel', style: TextStyle(color: Colors.blue)),
+      ],
+    ),
   );
 }
-
 
 Widget wallpaperList({List<WallpaperModel> wallpapers, context}) {
   return Container(
@@ -28,16 +27,18 @@ Widget wallpaperList({List<WallpaperModel> wallpapers, context}) {
       childAspectRatio: 0.6,
       mainAxisSpacing: 6.0,
       crossAxisSpacing: 6.0,
-      children: wallpapers.map((wallpaper){
+      children: wallpapers.map((wallpaper) {
         return GridTile(
           child: Container(
             child: ClipRRect(
                 borderRadius: BorderRadius.circular(16),
-                child: Image.network(wallpaper.src.portrait, fit: BoxFit.cover,)),
+                child: Image.network(
+                  wallpaper.src.portrait,
+                  fit: BoxFit.cover,
+                )),
           ),
         );
       }).toList(),
     ),
-
   );
 }

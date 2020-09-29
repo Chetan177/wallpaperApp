1163b8a62ccbb4a556d332b82ccc1134372f78f2
chetan177 <chetanpandey177@gmail.com>
2020-09-29 19:36:24 +0530

    factory method added in the wallpaer models

--- lib/model/wallpaper_model.dart
+++ lib/model/wallpaper_model.dart
@@ -1,19 +1,32 @@
-class WallpaperModel{
-
+class WallpaperModel {
   String photographer;
   String photographerURL;
   int photographerID;
   SrcModel src;
 
-  WallpaperModel({this.src, this.photographerURL, this.photographerID, this.photographer});
+  WallpaperModel(
+      {this.src, this.photographerURL, this.photographerID, this.photographer});
 
+  factory WallpaperModel.fromMap(Map<String, dynamic> jsonData) {
+    return WallpaperModel(
+        src: SrcModel.fromMap(jsonData["src"]),
+        photographerURL: jsonData["photographer_url"],
+        photographerID: jsonData["photographer_id"],
+        photographer: jsonData["photographer"]);
+  }
 }
 
-
-class SrcModel{
+class SrcModel {
   String original;
   String small;
   String portrait;
 
   SrcModel({this.portrait, this.original, this.small});
+
+  factory SrcModel.fromMap(Map<String, dynamic> jsonData) {
+    return SrcModel(
+        original: jsonData["original"],
+        portrait: jsonData["portrait"],
+        small: jsonData["small"]);
+  }
 }
